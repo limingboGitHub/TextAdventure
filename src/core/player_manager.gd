6bf33d806eb0b34bf8ccfe04403d25412d4803f9
mbli <675683354@qq.com>
2025-06-27 22:01:06 +0800

    fix: 卷轴使用时会额外减少

--- src/core/player_manager.gd
+++ src/core/player_manager.gd
@@ -178,8 +178,7 @@ func _on_item_used(item: DataBagItem):
 		if item.scroll != null:
 			# 使用卷轴
 			data_bag.ready_use_scroll(item)
-
-		if item.suit_scroll != null:
+		elif item.suit_scroll != null:
 			# 使用套装卷轴
 			data_bag.ready_use_suit_scroll(item)
 		else:
